90dd06e80ba868c08aa456fdd32b4fa9b5c61a56
Raphael Barros <finick021@gmail.com>
2026-05-23 21:27:47 -0300

    Delete PythonMess directory

--- PythonMess/RAD/Arquivos/02_ler_arquivo_txt.py
+++
@@ -1,5 +0,0 @@
-with open("mensagem2.txt", "r", encoding="utf-8") as arquivo:
-    conteudo = arquivo.read()
-
-print("Conteúdo do arquivo:")
-print(conteudo)
--- PythonMess/RAD/dadosSQL/01_conectar_e_criar_banco.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conexao = sqlite3.connect("escola.db")
-
-print("Banco de dados criado/conectado com sucesso.")
-
-conexao.close()
-
-print("Conexão encerrada.")
